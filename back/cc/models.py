68eacdb7554cf4d1d82a23f7b2fcab249f4293f7
Timur_hnimdvi <timurbakibayev@gmail.com>
2018-03-02 13:47:00 +0600

    models for chat

--- back/cc/models.py
+++ back/cc/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=1000, default="-")
+    firstContactTime = models.DateTimeField(auto_now_add=True)
+    ordering = models.IntegerField(default=0)
+    hidden = models.BooleanField(default=False)
+    phone_no = models.CharField(default="-", max_length=20)
+    device_id = models.CharField(max_length=1000)
+    device_type = models.CharField(max_length=1000, default="telegram")
+
+    def __str__(self):
+        return self.name + " " + self.device_type
+
+    class Meta:
+        ordering = ["ordering"]
+
+
+class ChatLine(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+    message = models.CharField(max_length=10000)
+    isReply = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.customer.name + " " + self.message + " "
+
+    class Meta:
+        ordering = ["time"]
